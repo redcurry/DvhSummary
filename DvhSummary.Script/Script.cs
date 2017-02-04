f96920efc5dad5e5cd3003b58078cafd6e54ff31
Carlos Anderson <carlosjanderson@gmail.com>
2017-02-04 14:26:18 -0500

    Add initial project files

---
+++ DvhSummary.Script/Script.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using VMS.TPS.Common.Model.API;
+
+namespace VMS.TPS
+{
+    public class Script
+    {
+        public void Execute(ScriptContext scriptContext, Window mainWindow)
+        {
+            Run(scriptContext.CurrentUser,
+                scriptContext.Patient,
+                scriptContext.Image,
+                scriptContext.StructureSet,
+                scriptContext.PlanSetup,
+                scriptContext.PlansInScope,
+                scriptContext.PlanSumsInScope,
+                mainWindow);
+        }
+
+        public void Run(
+            User user,
+            Patient patient,
+            Image image,
+            StructureSet structureSet,
+            PlanSetup planSetup,
+            IEnumerable<PlanSetup> planSetupsInScope,
+            IEnumerable<PlanSum> planSumsInScope,
+            Window mainWindow)
+        {
+            // Your main code now goes here
+        }
+    }
+}
